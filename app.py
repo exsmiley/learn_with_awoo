427f118867a64e675d44969a97b8f9c01afba917
ag8731 <ag8731@exeter.edu>
2018-03-04 01:52:37 -0500

    naming

--- app.py
+++ app.py
@@ -15,21 +15,22 @@ def home_page():
     return render_template('index.html')
 
 
-@app.route('/<string:catergory>')
-def catergory_list(catergory):
-	return list(data.keys())
+@app.route('/category')
+def category_list():
+    return list(data.keys())
 
 
-@app.route('/<string:catergory>/<string:name>')
-def topic_page(catergory, name):
+@app.route('/<string:name>')
+def topic_page(name):
     name = name.lower()
     issues_names = list(data[name].keys())
-    if len(issues_names) == 1:
-        return 'The current issue in {} is {}.'.format(name, issues_names[0])
+    return issues_names
+    # if len(issues_names) == 1:
+    #     return 'The current issue in {} is {}.'.format(name, issues_names[0])
 
-	    issues = ', '.join(issues_names[:-1]) + ', and ' + issues_names[-1]
+    #     issues = ', '.join(issues_names[:-1]) + ', and ' + issues_names[-1]
 
-    return 'The issues in {} are {}.'.format(name, issues)
+    # return 'The issues in {} are {}.'.format(name, issues)
 
 # if __name__ == '__main__':
 #     print(topic_page('Immigration'))
